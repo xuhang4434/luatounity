d1bdc4a6e67419fe0309cf81743ef6ccb9143741
xuhang4434 <1372641860@qq.com>
2021-12-07 19:34:43 +0800

    lua提交

--- Assets/Script/XFEngine/XFGame.cs
+++ Assets/Script/XFEngine/XFGame.cs
@@ -1,3 +1,4 @@
+using LuaInterface;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,15 @@ using UnityEngine;
 public class XFGame : MonoBehaviour
 {
     public static XFGame Instance = null;
+
+    //=============lua启动相关
+    protected LuaState luaState = null;
+    protected LuaLooper loop = null;
+    protected LuaFunction levelLoaded = null;
+
+    protected bool openLuaSocket = false;
+    protected bool beZbStart = false;
+    //=============lua启动相关
 
     private void Awake()
     {
@@ -17,7 +27,6 @@ public class XFGame : MonoBehaviour
     private void Start()
     {
         initSingletonManager();
-
         startMainLogic();
     }
 
@@ -34,6 +43,6 @@ public class XFGame : MonoBehaviour
     /// </summary>
     private void startMainLogic()
 	{
-
+        
 	}
 }
