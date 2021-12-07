28ac55951e6ae62a9d3d372974e6d88f775b431f
xuhang4434 <1372641860@qq.com>
2021-12-07 19:57:56 +0800

    修改

--- Assets/Script/XFEngine/XFGame.cs
+++ Assets/Script/XFEngine/XFGame.cs
@@ -1,7 +1,9 @@
 using LuaInterface;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class XFGame : MonoBehaviour
 {
@@ -22,16 +24,85 @@ public class XFGame : MonoBehaviour
         Application.runInBackground = true;
         Instance = this;
         GameObject.DontDestroyOnLoad(gameObject);
-    }	
+
+        SceneManager.sceneLoaded += onSceneLoaded;
+    }
+
+    private void onSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (levelLoaded != null)
+        {
+            levelLoaded.BeginPCall();
+            levelLoaded.Push(scene.buildIndex);
+            levelLoaded.PCall();
+            levelLoaded.EndPCall();
+        }
+
+        if (luaState != null)
+        {
+            luaState.RefreshDelegateMap();
+        }
+    }
 
     private void Start()
     {
+        //lua启动
+        luaStart();
+        
+        //初始化C#中的单例管理类
         initSingletonManager();
+
+        //开始lua的主逻辑main函数
         startMainLogic();
     }
 
     /// <summary>
-    /// 初始化单例管理类
+    /// lua启动
+    /// </summary>
+    private void luaStart()
+	{
+        if(LuaFileUtils.Instance == null)
+		{
+            return;
+		}
+
+        luaState = new LuaState();
+
+        luaState.OpenLibs(LuaDLL.luaopen_pb);
+        luaState.OpenLibs(LuaDLL.luaopen_struct);
+        luaState.OpenLibs(LuaDLL.luaopen_lpeg);
+        if (LuaConst.openLuaSocket)
+        {
+            luaState.BeginPreLoad();
+            luaState.RegFunction("socket.core", LuaOpen_Socket_Core);
+            luaState.RegFunction("mime.core", LuaOpen_Mime_Core);
+            luaState.EndPreLoad();
+        }
+        if (LuaConst.openLuaDebugger)
+        {
+            luaState.AddSearchPath(LuaConst.zbsDir);
+            luaState.LuaDoString(string.Format("DebugServerIp = '{0}'", "localhost"), "@LuaClient.cs");
+        }
+
+        luaState.LuaSetTop(0);
+        LuaBinder.Bind(luaState);
+        DelegateFactory.Init();
+        LuaCoroutine.Register(luaState, this);
+
+        luaState.Start();
+
+        loop = gameObject.AddComponent<LuaLooper>();
+        loop.luaState = luaState;
+
+        luaState.DoFile("XFEngine/Main.lua");
+        levelLoaded = luaState.GetFunction("OnLevelWasLoaded");
+        LuaFunction main = luaState.GetFunction("Main");
+        main.Call();
+        main.Dispose();
+        main = null;
+    }
+    /// <summary>
+    /// //初始化C#中的单例管理类
     /// </summary>
     private void initSingletonManager()
 	{
@@ -45,4 +116,19 @@ public class XFGame : MonoBehaviour
 	{
         
 	}
+
+
+
+    //======================================================================
+    [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+    static int LuaOpen_Socket_Core(IntPtr L)
+    {
+        return LuaDLL.luaopen_socket_core(L);
+    }
+
+    [MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+    static int LuaOpen_Mime_Core(IntPtr L)
+    {
+        return LuaDLL.luaopen_mime_core(L);
+    }
 }
