56b13b25b836a29fdad951f7f986441c811b93be
xuhang4434 <1372641860@qq.com>
2021-12-13 20:49:11 +0800

    提交

---
+++ Assets/Lua/Project/MyProject.lua
@@ -0,0 +1,23 @@
+MyProject = class("MyProject")
+
+function MyProject:ctor(...)
+    print("MyProject ctor")
+end
+
+function MyProject:Update()
+    
+end
+
+function MyProject:LateUpdate()
+ 
+end
+
+function MyProject:FixedUpdate()
+  
+end
+
+singleton_class(MyProject)
+
+
+
+
